99f2385cd00d334957a9f3f6c8af96112c3ff256
EduardoBadilla <41029312+EduardoBadilla@users.noreply.github.com>
2018-07-31 04:10:34 -0600

    Update ledpi.py

--- ledpi.py
+++ ledpi.py
@@ -2,9 +2,9 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
+GPIO.setup(25,GPIO.OUT)
 print "LED on"
-GPIO.output(18,GPIO.HIGH)
+GPIO.output(25,GPIO.HIGH)
 time.sleep(5)
 print "LED off"
-GPIO.output(18,GPIO.LOW)
+GPIO.output(25,GPIO.LOW)
